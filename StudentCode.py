dd27e70a579b93fec5b607b9182fc48032a82d79
AshvirDebba <77722001+AshvirDebba@users.noreply.github.com>
2021-01-20 17:48:31 +0200

    Add files via upload

---
+++ StudentCode.py
@@ -0,0 +1,9 @@
+#Code for Student
+
+#Prompt for student number
+prompt = "Enter student number: "
+stuNum = input(prompt)
+randNum = stuNum[5:9]
+
+#Basic Randomiser (add all digits divide by amount of numbers)
+for a in randNum:
