f49ce5334bae89433e0bce3af6ba97a3e0f437c5
Ash <ashvir.l.debba@gmail.com>
2021-01-20 18:16:18 +0200

    Foundation

--- StudentCode.py
+++ StudentCode.py
@@ -1,9 +1,11 @@
 #Code for Student
 
 #Prompt for student number
-prompt = "Enter student number: "
-stuNum = input(prompt)
-randNum = stuNum[5:9]
+prompt = "Enter student number: " #prompt user
+stuNum = input(prompt) #store student number
+randNum = stuNum[5:9] #take last 4 digits of number and use for randomising
 
 #Basic Randomiser (add all digits divide by amount of numbers)
 for a in randNum:
+    print(int(a))
+    
